6b7f4a8d123529d1959b8b6a112d897c2595bfb5
Erhard Siegl <erhard.siegl@gepardec.com>
2020-08-05 22:57:11 +0200

    Simple CDI Example

---
+++ solutions/simple-cdi/src/main/java/org/apache/camel/example/cdi/ApplicationCdiCamel.java
@@ -0,0 +1,10 @@
+package org.apache.camel.example.cdi;
+
+import org.apache.camel.cdi.Main;
+
+public class ApplicationCdiCamel {
+
+	    public static void main(String... args) throws Exception {
+	    	new Main().run(args);;
+	    }
+	}
