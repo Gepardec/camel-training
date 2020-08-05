fa70b8fe2a3761d4b61644f9a2d1944904d8e665
Erhard Siegl <erhard.siegl@gepardec.com>
2020-08-05 23:03:51 +0200

    Spring Routes Example

---
+++ solutions/simple-route/src/main/java/at/gepardec/training/camel/simple_route/ApplicationSpringCamel.java
@@ -0,0 +1,14 @@
+package at.gepardec.training.camel.simple_route;
+
+import org.apache.camel.spring.Main;
+
+public class ApplicationSpringCamel {
+
+    /**
+     * A main() so we can easily run these routing rules in our IDE
+     * @throws Exception 
+     */
+    public static void main(String... args) throws Exception {
+        Main.main(args);
+    }
+}
