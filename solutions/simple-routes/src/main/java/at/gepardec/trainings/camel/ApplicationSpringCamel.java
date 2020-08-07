37f6fd748d8790165f943ac6f07c1ee306aada67
Erhard Siegl <erhard.siegl@gepardec.com>
2020-08-08 01:04:45 +0200

    New simple-route example

---
+++ solutions/simple-routes/src/main/java/at/gepardec/trainings/camel/ApplicationSpringCamel.java
@@ -0,0 +1,10 @@
+package at.gepardec.trainings.camel;
+
+import org.apache.camel.spring.Main;
+
+public class ApplicationSpringCamel {
+
+     public static void main(String... args) throws Exception {
+        Main.main(args);
+    }
+}
---
+++ solutions/simple-routes/src/main/java/at/gepardec/trainings/camel/MyRouteBuilder.java
@@ -0,0 +1,19 @@
+package at.gepardec.trainings.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * A Camel Java DSL Router
+ */
+public class MyRouteBuilder extends RouteBuilder {
+
+    /**
+     * Let's configure the Camel routing rules using Java code...
+     */
+    public void configure() {
+
+        from("file:target/messages/others?noop=true")
+        .to("file:target/messages/somewhere");
+    }
+
+}
