030c883756da75a0d0c41e5a08c6d132b1e69bb0
Erhard Siegl <erhard.siegl@gepardec.com>
2020-09-09 20:34:08 +0200

    Main support for CDI-Application

---
+++ solutions/simple-cdi/src/main/java/at/gepardec/trainings/camel/ApplicationCdiCamel.java
@@ -0,0 +1,10 @@
+package at.gepardec.trainings.camel;
+
+import org.apache.camel.cdi.Main;
+
+public class ApplicationCdiCamel {
+
+	    public static void main(String... args) throws Exception {
+	    	new Main().run(args);;
+	    }
+	}
