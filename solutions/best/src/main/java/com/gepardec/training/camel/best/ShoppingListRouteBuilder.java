9a83e64c7f8f82b249bf1a8225fb6bb8292238db
cdjri <johannes.riegler@gepardec.com>
2020-10-20 08:24:24 +0200

    kick java 11 again

--- solutions/best/src/main/java/com/gepardec/training/camel/best/ShoppingListRouteBuilder.java
+++ solutions/best/src/main/java/com/gepardec/training/camel/best/ShoppingListRouteBuilder.java
@@ -1,7 +1,9 @@
 package com.gepardec.training.camel.best;
 
+import com.gepardec.training.camel.best.domain.ShoppingList;
 import com.gepardec.training.camel.commons.processor.ExceptionLoggingProcessor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
 
 import javax.enterprise.context.ApplicationScoped;
 
@@ -12,7 +14,12 @@ public class ShoppingListRouteBuilder extends RouteBuilder {
     public static final String ENTRY_SEDA_ENDOINT_URI = "seda:best_shoppingList_entry";
     public static final String ENTRY_SEDA_ENDOINT_ID = "best_shoppingList_entry";
 
+    public static final String ENTRY_SEDA_ENDOINT_URI_FROM_JSON = "seda:best_shoppingList_json_entry";
+    public static final String ENTRY_SEDA_ENDOINT_ID_FROM_JSON = "best_shoppingList_json_entry";
+
     public static final String ROUTE_ID = "ShoppingListRouteBuilder";
+    public static final String ROUTE_ID_FROM_JSON = "ShoppingListFromJsonRouteBuilder";
+
 
     @Override
     public void configure() {
@@ -23,6 +30,23 @@ public class ShoppingListRouteBuilder extends RouteBuilder {
         from(ENTRY_SEDA_ENDOINT_URI).id(ENTRY_SEDA_ENDOINT_ID)
                 .routeId(ROUTE_ID)
                 .marshal().json()
-                .to("file:src/test/resources/logs?filename=logfile.txt&fileExist=Append");
+                //file will be overwritten,can be avoided with "fileExists=append"
+                .to("file:src/test/resources/logs?filename=logfile.txt");
+
+
+        from("direct:triggerFileRoute")
+                .routeId("triggerFileRoute")
+                .log("test")
+                .to("file:src/test/resources/logs?filename=logfile.txt");
+
+        from("file:src/test/resources/logs?filename=logfile.txt")
+                .routeId(ROUTE_ID_FROM_JSON)
+                .autoStartup(false)
+                .unmarshal(new JacksonDataFormat(ShoppingList.class))
+                .process(exchange -> System.out.println())
+                .to(ENTRY_SEDA_ENDOINT_URI_FROM_JSON).id(ENTRY_SEDA_ENDOINT_ID_FROM_JSON);
+
+//        from("file-watch:src/test/resources/logs")
+//                .log("File event: ${header.CamelFileEventType} occurred on file ${header.CamelFileName} at ${header.CamelFileLastModified}");
     }
 }
--- solutions/best/src/test/java/com/gepardec/training/camel/best/ShoppingListRouteBuilderTest.java
+++ solutions/best/src/test/java/com/gepardec/training/camel/best/ShoppingListRouteBuilderTest.java
@@ -2,19 +2,25 @@ package com.gepardec.training.camel.best;
 
 import com.gepardec.training.camel.best.domain.ShoppingList;
 import com.gepardec.training.camel.best.domain.ShoppingListItem;
+import com.gepardec.training.camel.commons.test.routetest.MockedEndpointId;
 import com.gepardec.training.camel.commons.test.routetest.MockedRouteId;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.cdi.Uri;
+import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.cdi.Beans;
 import org.apache.camel.test.cdi.CamelCdiRunner;
+import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,11 +32,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ShoppingListRouteBuilderTest {
 
     @Inject
+    private CamelContext camelContext;
+
+    @Inject
     @Uri(ShoppingListRouteBuilder.ENTRY_SEDA_ENDOINT_URI)
     private ProducerTemplate producerTemplate;
 
+    @Inject
+    @Uri("direct:triggerFileRoute")
+    private ProducerTemplate producerTemplateTrigger;
+
+
+    @Inject
+    @Uri("mock:shopping_result")
+    @MockedEndpointId(ShoppingListRouteBuilder.ENTRY_SEDA_ENDOINT_ID_FROM_JSON)
+    private MockEndpoint shoppingResult;
+
+
+
     @Test
-    public void correctInput_messageInQueue() throws IOException {
+    public void writeIntoLogFileTest() throws IOException {
         List<ShoppingListItem> shoppingListItems = new ArrayList<>();
         shoppingListItems.add(new ShoppingListItem(2, 2));
         shoppingListItems.add(new ShoppingListItem(1, 10));
@@ -41,9 +62,28 @@ public class ShoppingListRouteBuilderTest {
         ShoppingList shoppingList = new ShoppingList(shoppingListItems, "basics");
 
         producerTemplate.sendBody(shoppingList);
-        String content = Files.readString(Path.of("src/test/resources/logs/logfile.txt"), StandardCharsets.US_ASCII);
+        String content = new String(Files.readAllBytes(Paths.get("src/test/resources/logs/logfile.txt")));
         assertThat(content).isNotEmpty();
         assertThat(content).contains("[{\"code\":2,\"amount\":2},{\"code\":1,\"amount\":10},{\"code\":3,\"amount\":2}");
     }
 
+    @Ignore
+    @Test
+    public void readFromLogFileTest() throws Exception {
+
+        producerTemplateTrigger.sendBody("test");
+        shoppingResult.expectedMessageCount(1);
+        shoppingResult.expects(() -> {
+            final Exchange exchange = shoppingResult.getExchanges().stream().findFirst().orElse(null);
+
+            Assert.assertNotNull(exchange);
+        });
+
+        // When
+        producerTemplate.start();
+
+        // Then
+        shoppingResult.assertIsSatisfied();
+    }
 }
+
