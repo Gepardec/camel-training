15954518b18f09ef7e44fe0c41646cdef86991c7
Erhard Siegl <erhard.siegl@gepardec.com>
2020-10-22 21:56:11 +0200

    Example for transformation and processor

--- solutions/simple-cdi/src/main/java/com/gepardec/trainings/camel/best/MyRoutes.java
+++ solutions/simple-cdi/src/main/java/com/gepardec/trainings/camel/best/MyRoutes.java
@@ -3,26 +3,62 @@ package com.gepardec.trainings.camel.best;
 import javax.inject.Inject;
 
 import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.cdi.Uri;
+import org.apache.camel.component.jackson.JacksonDataFormat;
+
+import com.gepardec.training.camel.best.domain.Order;
 
 
-public class MyRoutes extends RouteBuilder {
+public class MyRoutes extends AdviceWithRouteBuilder {
 
-    public static final String URL_FILE_ORDERS_IN = "file:src/orders?noop=true";
+	public static final String DIRECT_ORDER_IN = "direct:order_in";
+	public static final String URL_FILE_ORDERS_IN = "file:src/orders";
 	public static final String URL_FILE_ORDERS_OUT = "file:target/orders/processed";
 
-    @Inject
-    @Uri(URL_FILE_ORDERS_IN)
+ //   @Inject
+ //   @Uri(URL_FILE_ORDERS_IN)
     private Endpoint inputEndpoint;
 
-    @Inject
-    @Uri(URL_FILE_ORDERS_OUT)
+//    @Inject
+//    @Uri(URL_FILE_ORDERS_OUT)
     private Endpoint resultEndpoint;
 
     @Override
     public void configure() {
-        from(inputEndpoint)
-        .to(resultEndpoint);
+    	JacksonDataFormat orderFormat = new JacksonDataFormat(Order.class);
+
+        from(URL_FILE_ORDERS_IN)
+        .log("Process order ${body}")
+        .unmarshal(orderFormat)
+        .to(DIRECT_ORDER_IN);
+        
+        from(DIRECT_ORDER_IN)
+        .process(new Processor() {
+			
+			@Override
+			public void process(Exchange exchange) throws Exception {
+				exchange.getIn().getBody(Order.class).setPartnerId(34);
+			}
+		})
+        .marshal(orderFormat)
+        .log("Processed order ${body}")
+        .to(URL_FILE_ORDERS_OUT);
+  /*  	
+    	JacksonDataFormat orderFormat = new JacksonDataFormat(Order.class);
+    	
+        from(URL_FILE_ORDERS_IN)
+        .log("Process order ${body}")
+        .unmarshal(orderFormat)
+        .to(DIRECT_ORDER_IN);
+        
+        from(DIRECT_ORDER_IN)
+        .marshal(orderFormat)
+        .log("to file processed ep: ${body}")
+        .to(URL_FILE_ORDERS_OUT);
+        */
     }
 }
--- solutions/simple-cdi/src/test/java/com/gepardec/trainings/camel/best/OrderProcessingTest.java
+++ solutions/simple-cdi/src/test/java/com/gepardec/trainings/camel/best/OrderProcessingTest.java
@@ -1,8 +1,16 @@
 package com.gepardec.trainings.camel.best;
 
+import java.lang.reflect.Field;
+
+import javax.inject.Inject;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.cdi.Uri;
 import org.apache.camel.component.direct.DirectEndpoint;
 import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.reifier.RouteReifier;
 import org.apache.camel.test.cdi.CamelCdiRunner;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
@@ -12,32 +20,64 @@ import org.junit.runner.RunWith;
 @RunWith(CamelCdiRunner.class)
 public class OrderProcessingTest extends CamelTestSupport {
 
-
+    @Inject
+    protected CamelContext ctx;
+	
     @Test
     public void when_order_in_orders_message_is_in_processed() throws InterruptedException {
-        String orderIn = "{'partnerId': 1, 'items': [{ 'code': 1, 'amount': 110 }]}";
+        String orderIn = "{\"partnerId\": 1, \"items\": [{ \"code\": 1, \"amount\": 110 }]}";
+        String orderExpected = "{\"partnerId\":34,\"items\":[{\"code\":1,\"amount\":110}]}";
 
         MockEndpoint resultEndpoint = resolveMandatoryEndpoint("mock:result", MockEndpoint.class);
         resultEndpoint.expectedMessageCount(1);
-        resultEndpoint.expectedBodiesReceived(orderIn + "x");
-        
-        DirectEndpoint directOrderInEndpoint = resolveMandatoryEndpoint(MyRoutes.DIRECT_ORDER_IN, DirectEndpoint.class);
+        resultEndpoint.expectedBodiesReceived(orderExpected);
         
         
         template.sendBody("direct:start", orderIn);
         resultEndpoint.assertIsSatisfied();
     }
+/*
+    @Test
+    public void when_order_in_orders_message_is_in_direct_entpoint() throws Exception {
+    	        
+    	String orderIn = "{'partnerId': 1, 'items': [{ 'code': 1, 'amount': 110 }]}";
 
+    	AdviceWithRouteBuilder.adviceWith(ctx, "orderIn", in -> {
+            for (Field field : this.getClass().getDeclaredFields()) {
+                field.setAccessible(true);
+
+                    in.weaveById("direct_order_in")
+                            .replace()
+                            .to("direct:mocked_in");
+
+                }
+            });
+       getMockEndpoint("mock:direct_order_in").expectedMessageCount(1);
+        
+        
+        
+        template.sendBody(MyRoutes.URL_FILE_ORDERS_IN, orderIn);
+        assertMockEndpointsSatisfied();
+    }
+*/
     @Override
     protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
             public void configure() {
                 from("direct:start")
+                .log("Send to file order: ${body}")
                 .to(MyRoutes.URL_FILE_ORDERS_IN);
                                
                 from(MyRoutes.URL_FILE_ORDERS_OUT)
                 .to("mock:result");
             }
         };
+    }
+ 
+    @Override
+    public String isMockEndpoints() {
+        // override this method and return the pattern for which endpoints to mock.
+        // use * to indicate all
+        return "*";
     }
 }
