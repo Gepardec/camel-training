03ee756658ee2a35dec9ed35984aee85d74f8a77
Erhard Siegl <erhard.siegl@gepardec.com>
2024-03-25 15:08:54 +0100

    Reverted 467bc85712a7e01fa625

--- solutions/simple-cdi/src/main/java/com/gepardec/trainings/camel/best/MyRoutes.java
+++ solutions/simple-cdi/src/main/java/com/gepardec/trainings/camel/best/MyRoutes.java
@@ -1,8 +1,12 @@
 package com.gepardec.trainings.camel.best;
 
+import javax.inject.Inject;
+
+import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.cdi.Uri;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 
 import com.gepardec.training.camel.commons.domain.Order;
@@ -11,9 +15,18 @@ import com.gepardec.training.camel.commons.domain.Order;
 public class MyRoutes extends RouteBuilder {
 
 	public static final String DIRECT_BEAN_OTHER = "direct:beanOther";
+	public static final String OTHER_BEAN_ROUTE = "OTHER_BEAN_ROUTE";
 	public static final String DIRECT_ORDER_IN = "direct:order_in";
 	public static final String URL_FILE_ORDERS_IN = "file:target/orders/in";
 	public static final String URL_FILE_ORDERS_OUT = "file:target/orders/processed";
+
+    @Inject
+    @Uri(DIRECT_BEAN_OTHER)
+    private Endpoint directSomeBean;
+
+    @Inject
+    @Uri(MyRoutes.DIRECT_ORDER_IN)
+    private Endpoint fromEndpoint;
 
 
     @Override
@@ -21,9 +34,9 @@ public class MyRoutes extends RouteBuilder {
     	JacksonDataFormat orderFormat = new JacksonDataFormat(Order.class);
 
         from(URL_FILE_ORDERS_IN).routeId(URL_FILE_ORDERS_IN)
-        .to(DIRECT_ORDER_IN);
+        .to(fromEndpoint);
         
-        from(DIRECT_ORDER_IN)        
+        from(fromEndpoint)        
         .log("Process order ${body}")
         .unmarshal(orderFormat)
         .process(new Processor() {
@@ -36,9 +49,9 @@ public class MyRoutes extends RouteBuilder {
         .bean("someBean")
         .marshal(orderFormat)
         .log("Processed order ${body}")
-        .to(DIRECT_BEAN_OTHER);
+        .to(directSomeBean);
         
-        from(DIRECT_BEAN_OTHER).routeId(MyRoutes.DIRECT_BEAN_OTHER)
+        from(directSomeBean).routeId(MyRoutes.DIRECT_BEAN_OTHER)
         .log("Send to file: ${body}")
         .to(MyRoutes.URL_FILE_ORDERS_OUT);
  
